2ef7dad37bf1f3a092d06f12c8b1d9fa4eb61ddf
Austin Long <longaustin3@gmail.com>
2024-04-26 10:47:12 -0400

    This is Austins solution

--- 99_bottles.js
+++ 99_bottles.js
@@ -1,5 +1,23 @@
 function bottleSong() {
   // Write your code here!
+  for (let i = 99; i > 0; --i) {
+    let currNum = i;
+    let newNum = i - 1;
+    if (i > 2) {
+      console.log(`${currNum} bottles of beer on the wall, ${currNum} bottles of beer. 
+Take one down and pass it around, ${newNum} bottles of beer on the wall.`)
+    } else if (i === 2) {
+      console.log(`${currNum} bottles of beer on the wall, ${currNum} bottles of beer. 
+Take one down and pass it around, ${newNum} bottle of beer on the wall.`)
+    } else if (i === 1) {
+      console.log(`${currNum} bottle of beer on the wall, ${currNum} bottle of beer. 
+Take one down and pass it around, no more bottles of beer on the wall.
+No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.`)
+    } else {
+      break;
+    }
+  }
 };
 
-bottleSong();
+bottleSong(); 
