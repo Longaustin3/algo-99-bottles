299ca034b5fc27f94ca4dbf91abe6fe1ac5b0f3d
Austin Long <longaustin3@gmail.com>
2024-05-20 13:20:15 -0400

    solution

--- 99_bottles.py
+++ 99_bottles.py
@@ -1,5 +1,15 @@
-def bottle_song():
+def bottle_song(bottles):
 	# write your code here!
+	for x in range (bottles, -1, -1):
+		current_num = x
+		new_num = x - 1
+		if current_num > 2:
+			print(f'{current_num} bottles of beer on the wall, {current_num} bottles of beer. \nTake one down and pass it around, {new_num} bottles of beer on the wall.')
+		if current_num == 2:
+			print(f'{current_num} bottles of beer on the wall, {current_num} bottles of beer. \nTake one down and pass it around, {new_num} bottle of beer on the wall.')
+		if current_num == 1:
+			print(f'{current_num} bottle of beer on the wall, {current_num} bottle of beer. \nTake one down and pass it around, no more bottles of beer on the wall.')
+		if current_num == 0:
+			print(f'No more bottles of beer on the wall, no more bottles of beer. \nGo to the store and buy some more, {bottles} bottles of beer on the wall.')
 
-
-bottle_song()
+print(bottle_song(99))
